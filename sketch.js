7574f26bc8fca4d14f02b990367f0e76d292b305
sahanna007 <sahannananda@gmail.com>
2020-10-25 17:03:31 +0530

    made chain

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body, log4.body)
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
 }
